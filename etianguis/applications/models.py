84fff6d676ec7be1fc8abc860fed836650e99502
Oscar Roa <oshardy3@gmail.com>
2015-10-28 20:00:02 -0600

    Modelos creados

--- etianguis/applications/models.py
+++ etianguis/applications/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    appellido = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+class Producto(models.Model):
+    id_usuario = models.ForeignKey('Usuario')
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=500)
+    precio = models.PositiveIntegerField()
+
+class Transaccion(models.Model):
+    id_producto = models.ForeignKey('Producto')
+    id_usuario = models.ForeignKey('Usuario')
+    fecha = models.DateField()
+    cantidad = models.PositiveIntegerField()
