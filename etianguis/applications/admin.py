ae5ffb039f4e2d4e3e3b993bc726cc0ca51acd2a
Jorge Andrade <jorandradefig@gmail.com>
2015-10-28 21:47:57 -0600

    Showing Producto table in django dashboard

--- etianguis/applications/admin.py
+++ etianguis/applications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from applications.models import Producto
 
 # Register your models here.
+
+@admin.register(Producto)
+
+class ProductoAdmin(admin.ModelAdmin):
+  list_display = ('id', 'nombre','descripcion', 'imagen')
