0c3ee296b4dc222faa7f665536f1f5987c3925b6
Jorge Andrade <jorandradefig@gmail.com>
2015-10-29 21:24:20 -0600

    Retrieving changes

--- etianguis/applications/models.py
+++ etianguis/applications/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 class Producto(models.Model):
     id_usuario = models.ForeignKey(User, default=1)
