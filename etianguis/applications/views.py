3de842fde7c18b88416f21e61015310f95c7b2d8
Jorge Andrade <jorandradefig@gmail.com>
2015-10-30 21:19:09 -0600

    Setting urls.py

--- etianguis/applications/views.py
+++ etianguis/applications/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    template = loader.get_template('applications/index.html')
+    return HttpResponse(template.render(context))
