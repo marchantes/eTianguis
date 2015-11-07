9190a0904bcec2ae39f425129e6ee823fc97e409
Nekro Rockwell <urielnovoa@gmail.com>
2015-11-06 21:16:41 -0600

    Merge migrations

---
+++ etianguis/applications/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('applications', '0009_auto_20151107_0245'),
+        ('applications', '0010_auto_20151107_0312'),
+    ]
+
+    operations = [
+    ]
--- etianguis/applications/models.py
+++ etianguis/applications/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Producto(models.Model):
-    id_usuario = models.ForeignKey(User)
+    id_usuario = models.ForeignKey(User, default=1)
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=500)
     imagen = models.ImageField(upload_to='products',blank = True)
