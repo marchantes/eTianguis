1b5d21f48376cac84eed35f0aea6245e7fc188ca
Oscar Roa <oshardy3@gmail.com>
2015-11-13 20:57:56 -0600

    Required credentials to APIViews added

--- etianguis/api/views.py
+++ etianguis/api/views.py
@@ -2,19 +2,22 @@ from rest_framework import generics, permissions
 from applications.models import Producto
 from api.serializers import ProductoSerializer, UserSerializer
 from django.contrib.auth.models import User
+from api.permissions import IsOwnerOrReadOnly
 
 
 class ProductsList(generics.ListCreateAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(id_usuario=self.request.user)
 
 
 class ProductsDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
